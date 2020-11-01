3cd1a25ec12f7cddd37f6790f806af35e57664ea
abdelfatahh <abdelfatahhezema@gmail.com>
2020-11-01 10:21:15 +0200

    JSX composite components & expressions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt ME!"),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanes" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockteil",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt ME!"),
+  //   React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanes" }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockteil",
+  //   }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+  // ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockteil" />
+      <Pet name="Miaw" animal="Cat" breed="Tuna" />
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
