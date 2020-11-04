72ec2fba5d45cf2da37f768858f493b671554132
abdelfatahh <abdelfatahhezema@gmail.com>
2020-11-04 12:20:46 +0200

    adding context provider and theme context provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
   // return React.createElement("div", {}, [
@@ -15,17 +16,21 @@ const App = () => {
   //   }),
   //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
   // ]);
-
+  const themeHook = useState("peru");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
